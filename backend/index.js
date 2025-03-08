4dfa1a506573e41c2d0f2a4ad5d562e00c3ef1a4
Sayantan Nandi <officialsayantannandi@gmail.com>
2025-03-08 09:10:03 +0530

    MongoDB setup done

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const mongoose = require('mongoose');
+require('dotenv').config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -9,10 +11,17 @@ app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+const dbURI = process.env.MONGODB_URI;
+try{
+    mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true });
+    console.log('MongoDB connected');
+}catch(error){
+    console.log("MongoDB connection error",error);
+}
+
 app.get('/', (req, res) => {
     res.send('Express server is running');
 });
-
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
